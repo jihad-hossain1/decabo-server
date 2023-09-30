97a7ffd43eaa3609e906ee582b5fb5f2d1e42fe3
jihad hossain <jihadkhan934@gmail.com>
2023-10-01 01:17:33 +0600

    course crud oparation added

--- index.js
+++ index.js
@@ -26,6 +26,79 @@ async function run() {
         
         const courseCollection = client.db('Decabo').collection('courses')
 
+        const serchCollection = client.db('Decabo').collection('courses')
+        app.get('/getcourseId/:id')
+
+        const indexKeys = { title: 1, category: 1 };
+        const indexOptions = { category: "titleCategory" };
+        app.get("/getcourseText/:text", async (req, res) => {
+            const text = req.params.text;
+            const result = await serchCollection
+                .find({
+                    $or: [
+                        { title: { $regex: text, $options: "i" } },
+                        { category: { $regex: text, $options: "i" } },
+                        { date: { $regex: text, $options: "i" } },
+                    ],
+                })
+                .toArray();
+            res.send(result);
+        });
+        app.get('/courseByEmail', async (req, res) => {
+            let query = {};
+            if (req.query?.email) {
+                query = { email: req.query.email }
+            }
+            const result = await courseCollection.find(query).toArray();
+            res.send(result)
+        })
+        app.post("/course", async (req, res) => {
+            const doc = req.body;
+            const result = await courseCollection.insertOne(doc);
+            res.send(result);
+        });
+        //  collection api
+        app.get("/course", async (req, res) => {
+            const result = await courseCollection.find().toArray();
+            res.send(result);
+        });
+        // add a 
+
+        // get a single 
+        app.get('/course/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await courseCollection.findOne(query)
+            res.send(result)
+        })
+        app.get('/course/:email', async (req, res) => {
+            const email = req.params.email;
+            // const query = { _id: new ObjectId(id) }
+            const result = await courseCollection.find(email)
+            res.send(result)
+        })
+        // all geting card delete one by one
+        app.delete("/course/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await courseCollection.deleteOne(query);
+            res.send(result);
+        });
+
+        app.put('/course/:id', async (req, res) => {
+            const id = req.params.id;
+            const item = req.body
+            const filter = { _id: new ObjectId(id) };
+            const options = { upsert: true }
+            const updateDoc = {
+                $set: item,
+            }
+            const result = await courseCollection.updateOne(filter, updateDoc, options)
+            console.log(result);
+            res.send(result)
+        })
+       
+ 
         
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
